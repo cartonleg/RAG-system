f19323fba4bfe1f61197ae9e2e3b75b46c10ccfa
cartonleg <momaniammar44@gmail.com>
2025-02-01 22:21:26 +0300

    added os to base and dotenv to main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
 from routes import base
 
 app = FastAPI()
--- routes/base.py
+++ routes/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+import os
+
 
 base_router=APIRouter(prefix='/base')
 
 @base_router.get('/')
 def base_fun():
-    return 'hello there'
+    return 'hello there', os.getenv('APP_NAME')
