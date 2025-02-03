d0982868a385c938c7360bb32cc4880522bf1fac
cartonleg <momaniammar44@gmail.com>
2025-02-03 18:59:05 +0300

    removed dotenv from main

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
-import os
+from fastapi import APIRouter, Depends
+from helpers.config import get_settings, Settings
 
 
 base_router=APIRouter(prefix='/base')
 
 @base_router.get('/')
-def base_fun():
-    return 'hello there', os.getenv('APP_NAME')
+def base_fun(app_settings:Settings = Depends(get_settings)):
+    return 'hello there', app_settings.APP_NAME, app_settings.APP_VERSION
