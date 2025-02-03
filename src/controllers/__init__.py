38f52f3b654648e20dfb8862a0c19083517bbac2
cartonleg <momaniammar44@gmail.com>
2025-02-04 00:32:59 +0300

    created a way to either create a project file or find it's path if already exist in project controller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,2 +1,2 @@
-from .BaseController import BaseController
 from .DataController import DataController
+from .ProjectController import ProjectController
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 from controllers import DataController
 
@@ -9,4 +10,8 @@ data_router=APIRouter(prefix='/data')
 async def upload_data(project_id:str, file:UploadFile, 
                 app_settings:Settings = Depends(get_settings)):
     is_valid, reason = DataController().val_file(file=file)
+    if not is_valid:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, 
+                            content=f'status: {reason}')
     return f"is valid? {is_valid}'", f"status: {reason}"
+    
