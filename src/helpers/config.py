61e9c230820d65c24dd820c0bcea384f480f2219
cartonleg <momaniammar44@gmail.com>
2025-02-09 15:13:52 +0300

    added changes to config file too

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -3,9 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
+    
     ALLOWED_FILE_TYPE: list
     FILE_MAX_SIZE: int
     FILE_CHUNK_SIZE: int
+
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     model_config = SettingsConfigDict(env_file='.env')
 
