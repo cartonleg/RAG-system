a430d38c52801d7eb2c4f2f3dc65bc12c4ec81c4
cartonleg <momaniammar44@gmail.com>
2025-02-09 16:19:12 +0300

    created lifespan for project with database and created database scheme

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes import base, data
+from helpers.config import get_settings
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app):
+    app.mongo_conn = AsyncIOMotorClient(get_settings().MONGODB_URL)
+    app.db_client = app.mongo_conn[get_settings().MONGODB_DATABASE]
+
+    yield
+
+    app.mongo_conn.close()
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
+
