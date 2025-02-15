16ddb3c06a37822468b865ef3db14edc00bc3019
cartonleg <momaniammar44@gmail.com>
2025-02-15 17:46:37 +0300

    fixed typo

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -17,8 +17,8 @@ class ChunkModel(BaseDataModel):
 
     async def init_collection(self):
         all_collections = await self.db_client.list_collection_names()
-        if DataBaseEnum.COLLECTION_CHUNK_NAME.value not in all_collections:
-            self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNK_NAME.value]
+        if DataBaseEnum.COLLECTION_CHUNKS_NAME.value not in all_collections:
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNKS_NAME.value]
             indexes = DataChunk.get_indexes()
             for index in indexes:
                 await self.collection.create_index(index["key"], name=index["name"], 
