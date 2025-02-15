2fe7df2fd0c1a0c0315815381e42fe8efc8edcd8
cartonleg <momaniammar44@gmail.com>
2025-02-15 20:32:43 +0300

    asset model creation

--- src/models/enums/DataBaseEnum.py
+++ src/models/enums/DataBaseEnum.py
@@ -3,5 +3,6 @@ from enum import Enum
 class DataBaseEnum(Enum):
 
     COLLECTION_PROJECT_NAME = "projects"
-    COLLECTION_CHUNKS_NAME = "chunks"
+    COLLECTION_CHUNK_NAME = "chunks"
+    COLLECTION_ASSET_NAME = "assets"
     
--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,9 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 
 logger = logging.getLogger('uvicorn.error')
@@ -45,6 +47,13 @@ async def upload_data(request: Request, project_id:str, file:UploadFile,
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, 
                             content=ResponseSignal.UPLOAD_FAIL.value)
 
+    asset_model = await AssetModel.create_instance(db_client= request.app.db_client)
+
+    asset_resource = Asset(asset_project_id=project.id, asset_type=AssetTypeEnum.FILE.value, 
+                           asset_name=file_name, asset_size=os.path.getsize(file_path))
+    
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+    
     return JSONResponse(content=(ResponseSignal.UPLOAD_SUCCESS.value,
                                  ("file name = " + file_name)))
 
