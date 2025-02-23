337a5d1d6e12d20c964abcec81b557ce2c451b77
cartonleg <momaniammar44@gmail.com>
2025-02-23 18:50:44 +0300

    created the openAI provider

--- src/stores/LLMEnums.py
+++ src/stores/LLMEnums.py
@@ -5,3 +5,10 @@ class LLMEnums(Enum):
     OPENAI = "OPENAI"
     COHERE = "COHERE"
     
+
+class OpenAIEnums(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
+    
--- src/stores/LLMInterface.py
+++ src/stores/LLMInterface.py
@@ -7,16 +7,17 @@ class LLMInterface(ABC):
         pass
 
     @abstractmethod
-    def set_embedding_model(self, model_id: str):
+    def set_embedding_model(self, model_id: str, embedding_size: int):
         pass
 
     @abstractmethod
-    def generate_text(self, prompt: str, max_output_tokens: int,
-                      temprature: float = None):
+    def generate_text(self, prompt: str, chat_history: list=[], 
+                      max_output_tokens: int = None,
+                      temperature: float = None):
         pass
 
     @abstractmethod
-    def embed_text(self, text: str, document_type: str):
+    def embed_text(self, text: str, document_type: str = None):
         pass
 
     @abstractmethod
