e1889d2fb4a834ed1cf4d3ace860b9f322784c17
cartonleg <momaniammar95@gmail.com>
2025-05-23 22:06:35 +0300

    just removed something stupid I added long ago

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -96,7 +96,7 @@ class NLPController(BaseController):
             self.template_parser.get("rag", "document_prompt", {
                     "doc_num": idx + 1,
                     "chunk_text": self.generation_client.process_text(doc.text),
-            }) or "blah blah blah blah"
+            })
             for idx, doc in enumerate(retrieved_documents)
         ])
 
