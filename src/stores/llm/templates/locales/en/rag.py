b76dbe0e7b3a5534d7d14ebb19b1bd7611f8d20c
cartonleg <momaniammar95@gmail.com>
2025-05-25 19:37:17 +0300

    made the system prompts better

--- src/stores/llm/templates/locales/en/rag.py
+++ src/stores/llm/templates/locales/en/rag.py
@@ -8,13 +8,13 @@ you will be provided documents associated with the user's query
 
 generate a response using the provided documents, if the documents are irrelevant then ignore them.
 
-if there isn't enough documents then apologise to the user and tell him that the documents you have don't contain the answer to the user's query
+if there isn't enough documents then apologise to the user and tell him that the documents you have don't contain the answer to the user's query and don't provide an answer
 
-if for any other reason you couldn't generate a response then also apologise
+if for any other reason you couldn't generate a response then also apologise and don't provide an answer
 
 be friendly, answer with precision, don't give any information that is not helpful.
 
-respond in the language the query was given in
+respond in the language the query was given in.
 """)
 
 document_prompt = Template("""
